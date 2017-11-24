7d87994a222620501442544eef1d0c708331a672
Finn Poppinga <finno.ger@gmail.com>
2017-11-25 00:52:41 +0100

    typesafe all tasks query

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,19 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
-import {App} from "./app"
+import { App } from "./app"
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+const client = new ApolloClient({
+    link: new HttpLink({uri: "https://api.graph.cool/simple/v1/mopad"}),
+    cache: new InMemoryCache()
+});
 
 ReactDOM.render(
-	<App/>,
-	document.getElementById('root')
+    <ApolloProvider client={client}>
+        <App/>
+    </ApolloProvider>,
+    document.getElementById('root')
 );

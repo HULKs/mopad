085a383802e600a203eab54716a9f10918130fae
Finn Poppinga <finno.ger@gmail.com>
2017-11-25 12:40:24 +0100

    Add login logic
    * use a middleware (called link in apollo) to decorate the authentication token to all requests
    * create a login page, which logs in a user and saves the token to the local storage
    * require user to be authenticated in order to see talks
    * unify naming schema of graphql based components
    * rewrite props into sane format using the prop transformation of apollo

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { HashRouter as Router, Route } from "react-router-dom";
 import { Home } from "./pages/home/homePage";
+import LoginPage from "./pages/login/loginPage";
 
 export class App extends React.Component {
     public render() {
@@ -8,6 +9,7 @@ export class App extends React.Component {
             <Router>
                 <div>
                     <Route exact path="/" component={Home} />
+                    <Route exact path="/login" component={LoginPage} />
                 </div>
             </Router>
         );
---
+++ client/src/pages/login/loginForm.tsx
@@ -0,0 +1,111 @@
+import * as React from "react";
+import { FormattedMessage } from "react-intl";
+import { ISessionStore } from "../../business/auth";
+import gql from "graphql-tag";
+import { LoginMutation } from "../../../mopad-graphql";
+import graphql from "react-apollo/graphql";
+import { ApolloQueryResult } from "apollo-client";
+
+interface PublicProps {
+    sessionStore: ISessionStore;
+}
+
+interface LoginFormProps {
+    doLogin(
+        email: string,
+        password: string
+    ): Promise<ApolloQueryResult<LoginMutation>>;
+}
+
+interface LoginFormState {
+    email: string;
+    password: string;
+    showError: boolean;
+}
+
+export class DisconnectedLoginForm extends React.Component<
+    LoginFormProps & PublicProps,
+    LoginFormState
+> {
+    constructor(props) {
+        super(props);
+        this.handleLogin = this.handleLogin.bind(this);
+        this.state = {
+            email: null,
+            password: null,
+            showError: false
+        };
+    }
+
+    public render() {
+        return (
+            <div>
+                <label>
+                    <FormattedMessage id="app.login.email" />
+                    <input
+                        type="text"
+                        onChange={e => this.setState({ email: e.target.value })}
+                    />
+                </label>
+                <label>
+                    <FormattedMessage id="app.login.password" />
+                    <input
+                        type="password"
+                        onChange={e =>
+                            this.setState({ password: e.target.value })
+                        }
+                    />
+                </label>
+                <button type="submit" onClick={this.handleLogin}>
+                    <FormattedMessage id="app.login.login_cta" />
+                </button>
+                {this.renderError()}
+            </div>
+        );
+    }
+
+    private async handleLogin() {
+        try {
+            const response = await this.props.doLogin(
+                this.state.email,
+                this.state.password
+            );
+            this.props.sessionStore.token =
+                response.data.authenticateUser.token;
+        } catch (err) {
+            console.error("Error during login:", err);
+            this.setState({ showError: true });
+        }
+    }
+
+    private renderError() {
+        if (!this.state.showError) {
+            return null;
+        }
+
+        return (
+            <div>
+                <FormattedMessage id="app.login.error.message" />
+            </div>
+        );
+    }
+}
+
+const LOGIN_MUTATION = gql`
+    mutation Login($email: String!, $password: String!) {
+        authenticateUser(email: $email, password: $password) {
+            id
+            token
+        }
+    }
+`;
+
+export default graphql<LoginMutation, PublicProps, LoginFormProps>(
+    LOGIN_MUTATION,
+    {
+        props: ({ mutate }) => ({
+            doLogin: (email: string, password: string) =>
+                mutate({ variables: { password, email } })
+        })
+    }
+)(DisconnectedLoginForm);

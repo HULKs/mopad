103fca4ac069bc08129dde81c2917a3bbbb80d11
Finn Poppinga <finno.ger@gmail.com>
2017-11-25 12:40:24 +0100

    Add login logic
    * use a middleware (called link in apollo) to decorate the authentication token to all requests
    * create a login page, which logs in a user and saves the token to the local storage
    * require user to be authenticated in order to see talks
    * unify naming schema of graphql based components
    * rewrite props into sane format using the prop transformation of apollo

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,15 +5,23 @@ import { ApolloProvider } from "react-apollo";
 import { ApolloClient } from "apollo-client";
 import { HttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { from } from "apollo-link";
+import { AuthenticationLink, LocalSessionStore } from "./business/auth";
+import { IntlProvider } from "react-intl";
 
 const client = new ApolloClient({
-    link: new HttpLink({ uri: "https://api.graph.cool/simple/v1/mopad" }),
+    link: from([
+        new AuthenticationLink(new LocalSessionStore()),
+        new HttpLink({ uri: "https://api.graph.cool/simple/v1/mopad" })
+    ]),
     cache: new InMemoryCache()
 });
 
 ReactDOM.render(
-    <ApolloProvider client={client}>
-        <App />
-    </ApolloProvider>,
+    <IntlProvider locale="en" messages={require("./i18n/en-US.json")}>
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    </IntlProvider>,
     document.getElementById("root")
 );
--- client/src/pages/home/homePage.tsx
+++ client/src/pages/home/homePage.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
-import TalkList from "../../talks/TalkList";
+import { FormattedMessage, injectIntl } from "react-intl";
+import TalkList from "./TalkList";
 
 export class Home extends React.Component {
     public render() {
         return (
             <div>
-                <h1>Home</h1>
+                <h1>
+                    <FormattedMessage id="app.home" />
+                </h1>
                 <TalkList />
             </div>
         );

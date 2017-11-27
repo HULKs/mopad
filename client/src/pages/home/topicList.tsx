b09996ce67cd60aaf8f7ab83a542c57736ca78c4
Robert Oehlmann <waterfish33@gmail.com>
2017-11-27 22:43:59 +0100

    add server communication

--- client/src/pages/home/topicList.tsx
+++ client/src/pages/home/topicList.tsx
@@ -10,15 +10,20 @@ import {
 import FlatButton from "material-ui/FlatButton";
 import Topic from "./topic";
 import { FormattedMessage, injectIntl } from "react-intl";
+import { AllTopicsQuery } from "../../../mopad-graphql";
 
 interface PublicProps {
-    topics: any[];
+    topics: AllTopicsQuery["allTopics"];
 }
 
 export default class TopicList extends React.Component<PublicProps> {
     public render() {
         return (
-            <div>{this.props.topics.map(topic => <Topic topic={topic} />)}</div>
+            <div>
+                {this.props.topics.map(topic => (
+                    <Topic key={topic.id} topic={topic} />
+                ))}
+            </div>
         );
     }
 }

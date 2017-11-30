c9aff7aa5ff70ab4b60fd62e7728627cba39a80b
Robert Oehlmann <waterfish33@gmail.com>
2017-11-30 22:19:30 +0100

    generate css from less

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,8 +1,16 @@
 const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractLess = new ExtractTextPlugin({
+    filename: 'mopad.css'
+});
 
 module.exports = {
     devtool: 'inline-source-map',
-    entry: path.join(__dirname, "./src/index.tsx"),
+    entry: [
+        path.join(__dirname, "./src/index.tsx"),
+        path.join(__dirname, "./src/style/main.less")
+    ],
     output: {
         path: path.join(__dirname, "public"),
         filename: "index.js"
@@ -14,9 +22,16 @@ module.exports = {
     module: {
         rules: [
             // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-            { test: /\.tsx?$/, loader: 'ts-loader' }
+            { test: /\.tsx?$/, loader: 'ts-loader' },
+            {
+                test: /\.less$/,
+                use: extractLess.extract([ 'css-loader','less-loader' ])
+            }
         ]
     },
+    plugins: [
+        extractLess
+    ],
     devServer: {
         contentBase: path.join(__dirname, "public"),
         compress: true,

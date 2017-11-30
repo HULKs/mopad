d2c2a4a584be3e5cee2d2dae13339c34063b602e
Finn Poppinga <finno.ger@gmail.com>
2017-11-30 20:54:41 +0100

    centralize business logic

--- client/src/pages/home/homePage.tsx
+++ client/src/pages/home/homePage.tsx
@@ -4,8 +4,9 @@ import gql from "graphql-tag";
 import graphql from "react-apollo/graphql";
 import TopicList from "./topicList";
 import TopicAdd from "./topicAdd";
-import { AllTopicsQuery } from "../../../mopad-graphql";
+import { AddTopicMutation, AllTopicsQuery } from "../../../mopad-graphql";
 import { ApolloError } from "apollo-client";
+import { compose } from "react-apollo";
 
 interface PublicProps {}
 interface HomeProps {
@@ -16,6 +17,12 @@ interface HomeProps {
 type Props = PublicProps & HomeProps;
 
 export class Home extends React.Component<Props> {
+    constructor(props: Props) {
+        super(props);
+
+        this.onTopicAdd = this.onTopicAdd.bind(this);
+    }
+
     public render() {
         return (
             <div style={{ padding: "2em" }}>
@@ -23,11 +30,23 @@ export class Home extends React.Component<Props> {
                     <FormattedMessage id="app.home" />
                 </h1>
                 <TopicList topics={this.props.topics || []} />
-                <TopicAdd />
+                <TopicAdd onTopicAdd={this.onTopicAdd} />
             </div>
         );
     }
+
+    private onTopicAdd(title: string): void {
+        console.info(title);
+    }
 }
+
+const ADD_TOPIC_MUTATION = gql`
+    mutation AddTopic($title: String!, $description: String) {
+        createTopic(title: $title, description: $description) {
+            id
+        }
+    }
+`;
 
 const ALL_TOPICS_QUERY = gql`
     query AllTopics {
@@ -39,13 +58,21 @@ const ALL_TOPICS_QUERY = gql`
     }
 `;
 
-export default graphql<AllTopicsQuery, PublicProps, HomeProps>(
-    ALL_TOPICS_QUERY,
-    {
-        props: ({ data }) => ({
-            error: data.error,
-            loading: data.loading,
-            topics: data.allTopics
-        })
-    }
-)(Home);
+const addTopic = graphql<AddTopicMutation>(ADD_TOPIC_MUTATION, {
+    props: ({ mutate, ownProps }) => ({
+        ...ownProps,
+        addTopic: (title: string, description?: string) =>
+            mutate({ variables: { title, description } })
+    })
+});
+
+const loadTopic = graphql<AllTopicsQuery>(ALL_TOPICS_QUERY, {
+    props: ({ data, ownProps }) => ({
+        ...ownProps,
+        error: data.error,
+        loading: data.loading,
+        topics: data.allTopics
+    })
+});
+
+export default compose(addTopic, loadTopic)(Home);

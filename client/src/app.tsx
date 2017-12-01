4fd3e15962b2e8658578ec0ee982c0bffe97eabb
Robert Oehlmann <waterfish33@gmail.com>
2017-12-01 19:32:58 +0100

    extract menu

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -4,22 +4,40 @@ import { Route, Redirect } from "react-router";
 import AppBar from "material-ui/AppBar";
 import TopicPage from "./pages/home/topicPage";
 import LoginPage from "./pages/login/loginPage";
+import AppMenu from "./app/appMenu";
 import { PrivateRoute } from "./business/routerAuth";
 import { ISessionStore, LocalSessionStore } from "./business/auth";
 
-export class App extends React.Component {
+interface AppState {
+    menuOpen: boolean;
+}
+
+export class App extends React.Component<{}, AppState> {
     private sessionStore: ISessionStore;
 
     constructor(props) {
         super(props);
+        this.state = {
+            menuOpen: false
+        };
         this.sessionStore = new LocalSessionStore();
     }
 
     public render() {
+        const { menuOpen } = this.state;
         return (
             <Router>
                 <div>
-                    <AppBar title="mopad" />
+                    <AppBar
+                        title="mopad"
+                        onLeftIconButtonTouchTap={() =>
+                            this.setState({ menuOpen: !this.state.menuOpen })
+                        }
+                    />
+                    <AppMenu
+                        open={menuOpen}
+                        onClick={() => this.setState({ menuOpen: false })}
+                    />
                     <Route exact path="/login" component={LoginPage} />
                     <PrivateRoute
                         sessionStore={this.sessionStore}
@@ -40,7 +58,7 @@ export class App extends React.Component {
 }
 
 function LogoutPage() {
-	const sessionStore = new LocalSessionStore();
-	sessionStore.clearSession();
-	return (<Redirect to="/login" />);
+    const sessionStore = new LocalSessionStore();
+    sessionStore.clearSession();
+    return <Redirect to="/login" />;
 }
---
+++ client/src/app/appMenu.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import { RouterProps, withRouter } from "react-router";
+import { injectIntl, InjectedIntl } from "react-intl";
+import { compose } from "react-apollo";
+import Paper from "material-ui/Paper";
+import Menu from "material-ui/Menu";
+import MenuItem from "material-ui/MenuItem";
+
+interface AppMenuProps extends RouterProps {
+    onClick?();
+    open: boolean;
+    intl: InjectedIntl;
+}
+class DisconnectedAppMenu extends React.Component<AppMenuProps> {
+    constructor(props) {
+        super(props);
+        this.onSignOutClick = this.onSignOutClick.bind(this);
+    }
+
+    public render() {
+        const { open, intl } = this.props;
+        return (
+            <Paper style={{ position: "absolute", zIndex: 100 }}>
+                <Menu
+                    style={{ display: open ? "block" : "none" }}
+                    width={200}
+                    autoWidth={false}
+                >
+                    <MenuItem
+                        primaryText={intl.formatMessage({
+                            id: "app.menu.signOut"
+                        })}
+                        onClick={this.onSignOutClick}
+                    />
+                </Menu>
+            </Paper>
+        );
+    }
+
+    private onSignOutClick() {
+        this.props.history.push("/logout");
+        this.props.onClick();
+    }
+}
+
+export default compose(withRouter, injectIntl)(DisconnectedAppMenu);

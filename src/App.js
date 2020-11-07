a288491e40eae010a78b425af4e1ef3a59e4c21f
Maximilian Schmidt <maximilian@schmidt.so>
2020-11-07 11:51:51 +0100

    Use initial CardGrid

--- src/App.js
+++ src/App.js
@@ -1,39 +1,56 @@
 import React from "react";
-import 'semantic-ui-css/semantic.min.css'
+import "semantic-ui-css/semantic.min.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { Card, Icon, Image, Button, Label } from 'semantic-ui-react'
+import { Card, Icon, Image, Button, Label, Grid } from "semantic-ui-react";
 
-const nerds = ['TotallyNotThorsten', 'Thomas']
-const noobs = ['Yuria', 'Lasse', 'Maxi']
-const nerd_icon = 'graduation cap'
-const noob_icon = 'blind'
+const nerd_icon = "graduation cap";
+const noob_icon = "blind";
 
 // TODO: Don't allow is_nerd && is_noob
-const is_nerd = false
-const is_noob = true
+const is_nerd = false;
+const is_noob = false;
 
-const TalkCard = () => (
-
+const TalkCard = ({ card }) => (
   <Card>
-    <Card.Content header='My First Talk' />
-    <Card.Content description='I will talk about how I learned to talk.' />
+    <Card.Content header={card.title} />
+    <Card.Content description={card.description} />
     <Card.Content>
-      <Icon name={nerd_icon} /><b>Nerds</b>: {nerds.join(', ')}
+      <Icon name={nerd_icon} />
+      <b>Nerds</b>: {card.nerds.join(", ")}
       <br />
-      <Icon name={noob_icon} /><b>Noobs</b>: {noobs.join(', ')}
+      <Icon name={noob_icon} />
+      <b>Noobs</b>: {card.noobs.join(", ")}
     </Card.Content>
-    <Button.Group size='mini'>
+    <Button.Group size="mini">
       <Button toggle active={is_nerd}>
-        I'm a <span style={{marginLeft: 0.1 + 'em'}}><Icon name={nerd_icon} /></span>
+        I'm a{" "}
+        <span style={{ marginLeft: 0.1 + "em" }}>
+          <Icon name={nerd_icon} />
+        </span>
       </Button>
       <Button.Or />
-      <Button toggle active={is_noob} >
-        I'm a <Icon style={{marginLeft: 0.1 + 'em'}} name={noob_icon} />
+      <Button toggle active={is_noob}>
+        I'm a <Icon style={{ marginLeft: 0.1 + "em" }} name={noob_icon} />
       </Button>
-    </Button.Group >
+    </Button.Group>
   </Card>
-)
+);
+
+const CardGrid = ({ cards }) => (
+  <Grid>
+    {cards.map((card) => (
+      <TalkCard card={card} />
+    ))}
+  </Grid>
+);
 
 export default function App() {
-  return <TalkCard />
+  const example_card = {
+    title: "My second Talk",
+    description: "I am a description of a talk!",
+    nerds: ["TotallyNotThorsten", "Thomas"],
+    noobs: ["Yuria", "Lasse", "Maxi"],
+  };
+  const cards = [example_card, example_card, example_card];
+  return <CardGrid cards={cards} />;
 }

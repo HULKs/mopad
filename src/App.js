f4a0da43b87299ec287aeb42b2a79272be245a1d
ykonda <yuria.konda@gmail.com>
2020-11-07 19:06:07 +0100

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
-import SignUp from "./components/SignUp";
+import Register from "./components/Register";
 
 export default function App() {
-  return <SignUp/>;
+
+  return (
+    <Router>
+      <Switch>
+        <Route path='/login'>
+          <Login/>
+        </Route>
+        <Route path='/register'>
+          <Register/>
+        </Route>
+        <Route path='/'>
+          <Dashboard/>
+        </Route>
+      </Switch>
+    </Router>
+  );
 }

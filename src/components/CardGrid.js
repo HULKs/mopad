c6c124c791860bc85af5ceba7ded5c4bb6bc6663
Hendrik <dev.nipe.systems@gmail.com>
2020-11-07 17:27:51 +0100

    Improve cards

--- src/components/CardGrid.js
+++ src/components/CardGrid.js
@@ -1,21 +1,20 @@
 import React from "react";
-import { Card, Placeholder } from "semantic-ui-react";
+import { Card } from "semantic-ui-react";
 import TalkCard from "./TalkCard";
 import EditableCard from "./EditableCard";
 import firebase from "firebase";
-import { useCollection, useDocument } from "react-firebase-hooks/firestore";
+import { useCollection } from "react-firebase-hooks/firestore";
 
 export default function CardGrid() {
   const [talks, talksLoading, talksError] = useCollection(
-    firebase.firestore().collection("talks")
+    firebase.firestore().collection("talks"),
   );
   // TODO: loading, error
-  console.log(talks, talksLoading, talksError);
   if (talks !== undefined) {
     return (
       <Card.Group>
         {talks.docs.map(talk => (
-          <TalkCard talk={talk.data()} />
+          <TalkCard talkId={talk.id} />
         ))}
         <EditableCard />
       </Card.Group>

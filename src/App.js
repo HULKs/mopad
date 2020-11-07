9f66c92d509e0bfe027fc48983245579418c06d8
ykonda <yuria.konda@gmail.com>
2020-11-07 18:43:33 +0100

    Add sign up page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 
 export default function App() {
-  return <Login />;
+  return <SignUp/>;
 }

da55edebe644e7fec0c52621daf82061c8f79f1b
Hendrik <dev.nipe.systems@gmail.com>
2020-11-07 17:27:51 +0100

    Improve cards

--- src/components/TalkCard.js
+++ src/components/TalkCard.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Card, Icon, Button } from "semantic-ui-react";
+import firebase from "firebase";
 import { useCollection, useDocument } from "react-firebase-hooks/firestore";
 
 const nerd_icon = "graduation cap";
@@ -9,48 +10,58 @@ const noob_icon = "blind";
 const is_nerd = false;
 const is_noob = false;
 
-function useReferences(refs) {
-  const [values, setValues] = useState([])
+function useReferences(references, dependencies) {
+  const [values, setValues] = useState([]);
   useEffect(() => {
     (async () => {
-      const resolvedRefs = await Promise.all(refs.map((ref) => ref.get()));
-      setValues(resolvedRefs.map((it) => it.data()))
-    })()
-  }, [refs]);
+      const resolvedReferences = await Promise.all(references.map((reference) => reference.get()));
+      setValues(resolvedReferences.map(reference => reference.data()));
+    })();
+  }, dependencies);
   return values;
 }
 
-export default function TalkCard({ talk }) {
-  const nerds = useReferences(talk.nerds);
-  const noobs = useReferences(talk.noobs);
-
-  return (
-    <Card raised>
-      <Card.Content>
-        <Card.Header>{talk.title}</Card.Header>
-      </Card.Content>
-      <Card.Content style={{ height: 100 + "%" }}>
-        <Card.Description>{talk.description}</Card.Description>
-      </Card.Content>
-      <Card.Content>
-        <Icon name={nerd_icon} />
-        <b>Nerds</b>: { nerds.map((it) => it.name).join(', ') }
-        <br />
-        <Icon name={noob_icon} />
-        <b>Noobs</b>: { noobs.map((it) => it.name).join(', ')}
-      </Card.Content>
-      <Button.Group size="mini">
-        <Button toggle active={is_nerd}>
-          I'm a{" "}
-          <span style={{ marginLeft: 0.1 + "em" }}>
-            <Icon name={nerd_icon} />
-          </span>
-        </Button>
-        <Button.Or />
-        <Button toggle active={is_noob}>
-          I'm a <Icon style={{ marginLeft: 0.1 + "em" }} name={noob_icon} />
-        </Button>
-      </Button.Group>
-    </Card>
+export default function TalkCard({ talkId }) {
+  const [talk, talkLoading, talkError] = useDocument(
+    firebase.firestore().doc(`talks/${talkId}`),
   );
+  const nerds = useReferences(talk ? talk.data().nerds : [], [talk]);
+  const noobs = useReferences(talk ? talk.data().noobs : [], [talk]);
+
+  if (talk) {
+    return (
+      <Card raised>
+        <Card.Content>
+          <Card.Header>{talk.data().title}</Card.Header>
+        </Card.Content>
+        <Card.Content style={{ height: 100 + "%" }}>
+          <Card.Description>{talk.data().description}</Card.Description>
+        </Card.Content>
+        <Card.Content>
+          <Icon name={nerd_icon} />
+          <b>Nerds</b>: {nerds.map((it) => it.name).join(', ')}
+          <br />
+          <Icon name={noob_icon} />
+          <b>Noobs</b>: {noobs.map((it) => it.name).join(', ')}
+        </Card.Content>
+        <Button.Group size="mini">
+          <Button toggle active={is_nerd}>
+            I'm a{" "}
+            <span style={{ marginLeft: 0.1 + "em" }}>
+              <Icon name={nerd_icon} />
+            </span>
+          </Button>
+          <Button.Or />
+          <Button toggle active={is_noob}>
+            I'm a <Icon style={{ marginLeft: 0.1 + "em" }} name={noob_icon} />
+          </Button>
+        </Button.Group>
+      </Card>
+    );
+  }
+  // TODO: loading, error
+  return (
+    <>
+      Loading...
+    </>);
 }

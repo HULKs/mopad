edc6f013621888e6996390aa9f93d848eaf00031
Maximilian Schmidt <maximilian@schmidt.so>
2020-11-07 20:16:20 +0100

    Header as Header

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,23 +1,19 @@
 import React from "react";
 import CardGrid from "./CardGrid";
-import { Menu, Image } from "semantic-ui-react";
+import { Header, Segment, Icon } from "semantic-ui-react";
 
 export default function Dashboard() {
   return (
     <>
-      <Menu inverted>
-        <Menu.Item header>
-          <Image
-            size="mini"
-            src="./logo.png"
-            style={{ marginRight: "1.5em" }}
-          />
-          MOPAD
-        </Menu.Item>
-        <Menu.Item as="a" position="right" href='/login'>
+      <Segment clearing>
+        <Header as="h1" floated="left">
+          <Icon name="blind" size="medium" />
+          <Header.Content>MOPAD</Header.Content>
+        </Header>
+        <Header floated="right" block href="/login">
           Logout
-        </Menu.Item>
-      </Menu>
+        </Header>
+      </Segment>
       <CardGrid />
     </>
   );

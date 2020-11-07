c977a3d646e39f513d1672da0a5b16b082694d62
ykonda <yuria.konda@gmail.com>
2020-11-07 01:17:30 +0100

    Basic card design.

--- src/App.js
+++ src/App.js
@@ -1,70 +1,39 @@
 import React from "react";
 import 'semantic-ui-css/semantic.min.css'
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { Card, Icon, Image } from 'semantic-ui-react'
+import { Card, Icon, Image, Button, Label } from 'semantic-ui-react'
 
-const CardExampleCard = () => (
+const nerds = ['TotallyNotThorsten', 'Thomas']
+const noobs = ['Yuria', 'Lasse', 'Maxi']
+const nerd_icon = 'graduation cap'
+const noob_icon = 'blind'
+
+// TODO: Don't allow is_nerd && is_noob
+const is_nerd = false
+const is_noob = true
+
+const TalkCard = () => (
+
   <Card>
-    <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+    <Card.Content header='My First Talk' />
+    <Card.Content description='I will talk about how I learned to talk.' />
     <Card.Content>
-      <Card.Header>Matthew</Card.Header>
-      <Card.Meta>
-        <span className='date'>Joined in 2015</span>
-      </Card.Meta>
-      <Card.Description>
-        Matthew is a musician living in Nashville.
-      </Card.Description>
+      <Icon name={nerd_icon} /><b>Nerds</b>: {nerds.join(', ')}
+      <br />
+      <Icon name={noob_icon} /><b>Noobs</b>: {noobs.join(', ')}
     </Card.Content>
-    <Card.Content extra>
-        Hello World
-    </Card.Content>
+    <Button.Group size='mini'>
+      <Button toggle active={is_nerd}>
+        I'm a <span style={{marginLeft: 0.1 + 'em'}}><Icon name={nerd_icon} /></span>
+      </Button>
+      <Button.Or />
+      <Button toggle active={is_noob} >
+        I'm a <Icon style={{marginLeft: 0.1 + 'em'}} name={noob_icon} />
+      </Button>
+    </Button.Group >
   </Card>
 )
 
 export default function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
-}
-
-function Home() {
-  return <CardExampleCard></CardExampleCard>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
+  return <TalkCard />
 }

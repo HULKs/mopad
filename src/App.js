cc00a632c74acbe2c8121a437a87baefd21de874
Maximilian Schmidt <maximilian@schmidt.so>
2020-11-07 12:14:54 +0100

    Declare functions instead of arrow functions

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "semantic-ui-css/semantic.min.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import {
   List,
+  Segment,
   Card,
   Icon,
   Image,
@@ -18,43 +19,47 @@ const noob_icon = "blind";
 const is_nerd = false;
 const is_noob = false;
 
-const TalkCard = ({ card }) => (
-  <Card>
-    <Card.Content header={card.title} />
-    <Card.Content description={card.description} />
-    <Card.Content>
-      <Icon name={nerd_icon} />
-      <b>Nerds</b>: {card.nerds.join(", ")}
-      <br />
-      <Icon name={noob_icon} />
-      <b>Noobs</b>: {card.noobs.join(", ")}
-    </Card.Content>
-    <Button.Group size="mini">
-      <Button toggle active={is_nerd}>
-        I'm a{" "}
-        <span style={{ marginLeft: 0.1 + "em" }}>
-          <Icon name={nerd_icon} />
-        </span>
-      </Button>
-      <Button.Or />
-      <Button toggle active={is_noob}>
-        I'm a <Icon style={{ marginLeft: 0.1 + "em" }} name={noob_icon} />
-      </Button>
-    </Button.Group>
-  </Card>
-);
+function TalkCard({ card }) {
+  return (
+    <Card>
+      <Card.Content header={card.title} />
+      <Card.Content description={card.description} />
+      <Card.Content>
+        <Icon name={nerd_icon} />
+        <b>Nerds</b>: {card.nerds.join(", ")}
+        <br />
+        <Icon name={noob_icon} />
+        <b>Noobs</b>: {card.noobs.join(", ")}
+      </Card.Content>
+      <Button.Group size="mini">
+        <Button toggle active={is_nerd}>
+          I'm a{" "}
+          <span style={{ marginLeft: 0.1 + "em" }}>
+            <Icon name={nerd_icon} />
+          </span>
+        </Button>
+        <Button.Or />
+        <Button toggle active={is_noob}>
+          I'm a <Icon style={{ marginLeft: 0.1 + "em" }} name={noob_icon} />
+        </Button>
+      </Button.Group>
+    </Card>
+  );
+}
 
-const CardGrid = ({ cards }) => (
-  <List horizontal>
-    {cards.map((card) => (
-      <List.Item>
-        <TalkCard card={card} />
-      </List.Item>
-    ))}
-  </List>
-);
+function CardGrid({ cards }) {
+  return (
+    <List horizontal>
+      {cards.map((card) => (
+        <List.Item>
+          <TalkCard card={card} />
+        </List.Item>
+      ))}
+    </List>
+  );
+}
 
-export default function App() {
+function Page() {
   const example_card = {
     title: "My second Talk",
     description: "I am a description of a talk!",
@@ -62,5 +67,13 @@ export default function App() {
     noobs: ["Yuria", "Lasse", "Maxi"],
   };
   const cards = [example_card, example_card, example_card];
-  return <CardGrid cards={cards} />;
+  return (
+    <Segment placeholder>
+      <CardGrid cards={cards} />
+    </Segment>
+  );
+}
+
+export default function App() {
+  return <Page />;
 }

08302e858d3fb3214dc36f4f6d62068c4bfcb07b
Maximilian Schmidt <maximilian@schmidt.so>
2020-11-08 10:31:37 +0100

    restyle textarea in editablecard

--- src/components/EditableCard.js
+++ src/components/EditableCard.js
@@ -6,39 +6,55 @@ import { useAuthState } from "react-firebase-hooks/auth";
 export default function EditableCard() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
-  const [user,,] = useAuthState(firebase.auth());
+  const [user, ,] = useAuthState(firebase.auth());
   // TODO: user loading, user error
   return (
     <Card inverted>
       <Card.Content>
         <Card.Header>
-          <Input transparent fluid placeholder="Talk Topic..." value={title} onChange={e => setTitle(e.target.value)} />
+          <Input
+            transparent
+            fluid
+            placeholder="Talk Topic..."
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </Card.Header>
       </Card.Content>
       <Card.Content>
         <Card.Description>
           <Form>
-            <TextArea placeholder="What's your talk about?" value={description} onChange={e => setDescription(e.target.value)} />
+            <TextArea
+              style={{ border: "none", padding: 0 }}
+              placeholder="What's your talk about?"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
           </Form>
         </Card.Description>
       </Card.Content>
       <Card.Content>
-        <Button onClick={async () => {
-          await firebase.firestore().collection("talks").add({
-            creator: firebase.firestore().doc(`users/${user.uid}`),
-            description: description,
-            location: "Raum A",
-            nerds: [
-              firebase.firestore().doc(`users/${user.uid}`),
-            ],
-            noobs: [],
-            time: new Date(),
-            title: title,
-            type: "discussion",
-          });
-          setTitle("");
-          setDescription("");
-        }}>Create</Button>
+        <Button
+          onClick={async () => {
+            await firebase
+              .firestore()
+              .collection("talks")
+              .add({
+                creator: firebase.firestore().doc(`users/${user.uid}`),
+                description: description,
+                location: "Raum A",
+                nerds: [firebase.firestore().doc(`users/${user.uid}`)],
+                noobs: [],
+                time: new Date(),
+                title: title,
+                type: "discussion",
+              });
+            setTitle("");
+            setDescription("");
+          }}
+        >
+          Create
+        </Button>
       </Card.Content>
     </Card>
   );

a183beac417046348bcc2ffce875eb2d5a087030
Maximilian Schmidt <maximilian@schmidt.so>
2020-11-08 10:27:25 +0100

    Prettier

--- src/components/CardGrid.js
+++ src/components/CardGrid.js
@@ -7,13 +7,13 @@ import { useCollection } from "react-firebase-hooks/firestore";
 
 export default function CardGrid() {
   const [talks, ,] = useCollection(
-    firebase.firestore().collection("talks").orderBy("time"),
+    firebase.firestore().collection("talks").orderBy("time")
   );
   // TODO: loading, error
   if (talks !== undefined) {
     return (
       <Card.Group stackable>
-        {talks.docs.map(talk => (
+        {talks.docs.map((talk) => (
           <TalkCard talkId={talk.id} />
         ))}
         <EditableCard />

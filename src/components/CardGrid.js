cc934c39666ddb309c08c5e083c1b8d75898492b
Maximilian Schmidt <maximilian@schmidt.so>
2020-11-08 20:29:13 +0100

    toMillis on date objects

--- src/components/CardGrid.js
+++ src/components/CardGrid.js
@@ -4,31 +4,41 @@ import TalkCard from "./TalkCard";
 import EditableCard from "./EditableCard";
 
 export default function CardGrid({ user, users, talks }) {
-  const scheduledTalkIds = Object
-    .keys(talks)
-    .filter(talkId => "scheduled_at" in talks[talkId])
-    .sort((talkIdA, talkIdB) => talks[talkIdA].scheduled_at > talks[talkIdB].scheduled_at);
-  const unscheduledTalkIds = Object
-    .keys(talks)
-    .filter(talkId => !("scheduled_at" in talks[talkId]))
-    .sort((talkIdA, talkIdB) => talks[talkIdA].created_at > talks[talkIdB].created_at);
+  const scheduledTalkIds = Object.keys(talks)
+    .filter((talkId) => "scheduled_at" in talks[talkId])
+    .sort(
+      (talkIdA, talkIdB) =>
+        talks[talkIdA].scheduled_at.toMillis() >
+        talks[talkIdB].scheduled_at.toMillis()
+    );
+  const unscheduledTalkIds = Object.keys(talks)
+    .filter((talkId) => !("scheduled_at" in talks[talkId]))
+    .sort(
+      (talkIdA, talkIdB) =>
+        talks[talkIdA].created_at.toMillis() >
+        talks[talkIdB].created_at.toMillis()
+    );
 
   return (
     <Card.Group stackable>
-      {scheduledTalkIds.map((talkId) => <TalkCard
-        key={talkId}
-        talkId={talkId}
-        talk={talks[talkId]}
-        user={user}
-        users={users}
-      />)}
-      {unscheduledTalkIds.map((talkId) => <TalkCard
-        key={talkId}
-        talkId={talkId}
-        talk={talks[talkId]}
-        user={user}
-        users={users}
-      />)}
+      {scheduledTalkIds.map((talkId) => (
+        <TalkCard
+          key={talkId}
+          talkId={talkId}
+          talk={talks[talkId]}
+          user={user}
+          users={users}
+        />
+      ))}
+      {unscheduledTalkIds.map((talkId) => (
+        <TalkCard
+          key={talkId}
+          talkId={talkId}
+          talk={talks[talkId]}
+          user={user}
+          users={users}
+        />
+      ))}
       <EditableCard user={user} />
     </Card.Group>
   );

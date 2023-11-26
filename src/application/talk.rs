7b4a67611e0da0765d1d1a38fce433aae89afa7e
Hendrik <git@h3ndrk.de>
2023-11-26 19:42:34 +0100

    Require capabilities in talk service triggers

--- src/application/talk.rs
+++ src/application/talk.rs
@@ -1,13 +1,60 @@
-use std::time::{Duration, SystemTime};
+use std::{
+    collections::HashSet,
+    time::{Duration, SystemTime},
+};
 
 use async_trait::async_trait;
 use sqlx::Error;
 use tokio::sync::broadcast::Receiver;
 
+use super::authentication::Capability;
+
 #[async_trait]
 pub trait TalkService {
+    async fn trigger(
+        &self,
+        user_id: i64,
+        user_capabilities: &HashSet<Capability>,
+        command: Command,
+    ) -> Result<(), Error>;
     async fn register_for_updates(&self) -> Receiver<Update>;
-    async fn trigger(&self, command: Command) -> Result<(), Error>;
+}
+
+pub enum Command {
+    AddTalk {
+        title: String,
+        description: String,
+        duration: Duration,
+    },
+    RemoveTalk {
+        id: i64,
+    },
+    UpdateTitle {
+        id: i64,
+        title: String,
+    },
+    UpdateDescription {
+        id: i64,
+        description: String,
+    },
+    UpdateScheduledAt {
+        id: i64,
+        scheduled_at: Option<SystemTime>,
+    },
+    UpdateDuration {
+        id: i64,
+        duration: Duration,
+    },
+    UpdateLocation {
+        id: i64,
+        location: Option<String>,
+    },
+    ToggleNerd {
+        id: i64,
+    },
+    ToggleNoob {
+        id: i64,
+    },
 }
 
 pub enum Update {
@@ -52,43 +99,6 @@ pub enum Update {
     UpdateNoobs {
         id: i64,
         noobs: Vec<User>,
-    },
-}
-
-pub enum Command {
-    AddTalk {
-        title: String,
-        description: String,
-        duration: Duration,
-    },
-    RemoveTalk {
-        id: i64,
-    },
-    UpdateTitle {
-        id: i64,
-        title: String,
-    },
-    UpdateDescription {
-        id: i64,
-        description: String,
-    },
-    UpdateScheduledAt {
-        id: i64,
-        scheduled_at: Option<SystemTime>,
-    },
-    UpdateDuration {
-        id: i64,
-        duration: Duration,
-    },
-    UpdateLocation {
-        id: i64,
-        location: Option<String>,
-    },
-    TriggerNerd {
-        id: i64,
-    },
-    TriggerNoob {
-        id: i64,
     },
 }
 
